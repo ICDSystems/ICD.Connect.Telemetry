01ef528e352e44e3cc016467b8392d785efacd82
Jack Kanarish <jack.kanarish@wedoresi.com>
2019-04-10 17:21:10 -0400

    feat: dsp telemetry

---
+++ ICD.Connect.Telemetry.Crestron/SigMappings/IcdDspFusionSigs.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using ICD.Common.Utils.Collections;
+using ICD.Connect.Audio;
+using ICD.Connect.Devices.Controls;
+using ICD.Connect.Protocol.Sigs;
+
+namespace ICD.Connect.Telemetry.Crestron.SigMappings
+{
+	public static class IcdDspFusionSigs
+	{
+		public static IEnumerable<IFusionSigMapping> Sigs { get { return s_Sigs; } }
+
+		private static readonly IcdHashSet<IFusionSigMapping> s_Sigs = new IcdHashSet<IFusionSigMapping>
+		{
+			new FusionSigMapping
+			{
+				TelemetryGetName = DspTelemetryNames.FIRMWARE_VERSION,
+				TelemetrySetName = "",
+				FusionSigName = "Firmware Version",
+				Sig = 300,
+				SigType = eSigType.Serial
+			},
+			new FusionSigMapping
+			{
+				TelemetryGetName = DspTelemetryNames.CALL_ACTIVE,
+				TelemetrySetName = "",
+				FusionSigName = "ATC Call Active",
+				Sig = 301,
+				SigType = eSigType.Digital
+			},
+			new FusionSigMapping
+			{
+				TelemetryGetName = DspTelemetryNames.ACTIVE_FAULTS,
+				TelemetrySetName = "",
+				FusionSigName = "Active Faults",
+				Sig = 300,
+				SigType = eSigType.Digital
+			},
+			new FusionSigMultiMapping
+			{
+				TelemetryGetName = ControlTelemetryNames.NAME,
+				TelemetrySetName = "",
+				FusionSigName = "Volume Control {0} Name",
+				FirstSig = 10000,
+				LastSig = 10999,
+				SigType = eSigType.Serial
+			},
+			new FusionSigMultiMapping
+			{
+				TelemetryGetName = VolumeTelemetryNames.VOLUME_CONTROL_LEVEL,
+				TelemetrySetName = VolumeTelemetryNames.VOLUME_CONTROL_LEVEL_COMMAND,
+				FusionSigName = "Volume Control {0} Level",
+				FirstSig = 10000,
+				LastSig = 10999,
+				SigType = eSigType.Analog
+			},
+			new FusionSigMultiMapping
+			{
+				TelemetryGetName = VolumeTelemetryNames.VOLUME_CONTROL_MUTE,
+				TelemetrySetName = VolumeTelemetryNames.VOLUME_CONTROL_MUTE_COMMAND,
+				FusionSigName = "Volume Control {0} Mute",
+				FirstSig = 10000,
+				LastSig = 10999,
+				SigType = eSigType.Digital
+			}
+		};
+	}
+}
