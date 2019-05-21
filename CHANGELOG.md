25501ae95c983ae5846cf8557cbdd6ccdebd79ef
Chris Cameron <chris.cameron@wedoresi.com>
2019-05-21 09:52:37 -0400

    feat: Added util method for converting a log item into fusion log text format

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ### Added
  - FusionTelemetryMunger supports registering multiple mapping sets under existing mapping types
+ - Added util method for converting a log item into fusion log text format
 
 ## [5.0.0] - 2019-05-16
 ### Added
---
+++ ICD.Connect.Telemetry.Crestron/Utils/FusionUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using ICD.Common.Properties;
+using ICD.Common.Utils.Services.Logging;
+
+namespace ICD.Connect.Telemetry.Crestron.Utils
+{
+	public static class FusionUtils
+	{
+		/// <summary>
+		/// Return the text format to send to Fusion
+		/// </summary>
+		/// <returns>Text format for fusion, including timestamp, severity, and message</returns>
+		[PublicAPI]
+		public static string GetFusionLogText(LogItem logItem)
+		{
+			return GetFusionLogText(logItem.Timestamp, logItem.Severity, logItem.Message);
+		}
+
+		/// <summary>
+		/// Return the text format to send to Fusion
+		/// </summary>
+		/// <returns>Text format for fusion, including timestamp, severity, and message</returns>
+		[PublicAPI]
+		public static string GetFusionLogText(DateTime timestamp, eSeverity severity, string message)
+		{
+			StringBuilder s = new StringBuilder();
+			{
+				s.Append(timestamp.ToString("yyyyMMddHHmmss"));
+				s.Append("||");
+				s.Append((int)severity);
+				s.Append("||");
+				s.Append(message);
+			}
+			return s.ToString();
+		}
+	}
+}
