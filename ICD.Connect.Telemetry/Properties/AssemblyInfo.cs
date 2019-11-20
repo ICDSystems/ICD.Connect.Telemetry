2e227713dc08a364352a1ab706d91f60a943fa9e
Chris Cameron <chris.cameron@wedoresi.com>
2019-11-20 15:28:17 -0500

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Telemetry/Properties/AssemblyInfo.cs
+++ ICD.Connect.Telemetry/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Telemetry")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("5.2.2.0")]
+[assembly: AssemblyVersion("5.3.0.0")]
