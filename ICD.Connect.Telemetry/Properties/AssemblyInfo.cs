eef82d1c12e758dcf5daa8ab93edfbc3f2b5d494
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-03 10:28:32 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Telemetry/Properties/AssemblyInfo.cs
+++ ICD.Connect.Telemetry/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Telemetry")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Telemetry")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("5.3.0.0")]
