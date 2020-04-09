0fa162f710a8b4633029cae5a0fbf22cac082b8d
Jack Kanarish <jack.kanarish@wedoresi.com>
2020-04-09 13:45:57 -0400

    feat: create attribute that allows an object to identify what property should be used to index it in collection telemetry

---
+++ ICD.Connect.Telemetry/Attributes/TelemetryCollectionIdentityAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Crestron.SimplSharp.Reflection;
+using ICD.Common.Properties;
+using ICD.Common.Utils.Attributes;
+using ICD.Common.Utils.Extensions;
+
+namespace ICD.Connect.Telemetry.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+	public sealed class TelemetryCollectionIdentityAttribute : AbstractIcdAttribute
+	{
+		[CanBeNull]
+		public static PropertyInfo GetProperty([NotNull] object instance)
+		{
+			// todo - cache
+			return
+				instance.GetType()
+						.GetAllTypes()
+#if SIMPLSHARP
+						.Select(t => t.GetCType())
+#endif
+						.SelectMany(t => t.GetProperties())
+						.SingleOrDefault(p => p.GetCustomAttributes(typeof(TelemetryCollectionIdentityAttribute), true).Any());
+		}
+	}
+}
