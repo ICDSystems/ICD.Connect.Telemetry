2d3d96440e3ea700f44e98335c392f0f7f25ec41
Chris Cameron <chris.cameron@wedoresi.com>
2020-04-14 19:24:50 -0400

    feat: Added abstractions for external telemetry

---
+++ ICD.Connect.Telemetry/AbstractExternalTelemetryProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace ICD.Connect.Telemetry
+{
+	public abstract class AbstractExternalTelemetryProvider<TParent> : AbstractTelemetryProvider, IExternalTelemetryProvider
+		where TParent : ITelemetryProvider
+	{
+		private TParent m_Provider;
+
+		/// <summary>
+		/// Gets the parent telemetry provider.
+		/// </summary>
+		protected TParent Parent { get { return m_Provider; } }
+
+		/// <summary>
+		/// Sets the parent telemetry provider that this instance extends.
+		/// </summary>
+		/// <param name="provider"></param>
+		void IExternalTelemetryProvider.SetParent(ITelemetryProvider provider)
+		{
+			SetParent((TParent)provider);
+		}
+
+		/// <summary>
+		/// Sets the parent telemetry provider that this instance extends.
+		/// </summary>
+		/// <param name="provider"></param>
+		public void SetParent(TParent provider)
+		{
+			m_Provider = provider;
+		}
+	}
+}
---
+++ ICD.Connect.Telemetry/AbstractTelemetryProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace ICD.Connect.Telemetry
+{
+	public abstract class AbstractTelemetryProvider : ITelemetryProvider
+	{
+	}
+}
