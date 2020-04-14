89f02c722667efca2ee7cc3430d804025bea6d07
Chris Cameron <chris.cameron@wedoresi.com>
2020-04-14 18:29:26 -0400

    perf: Caching telemetry identifier properties

--- ICD.Connect.Telemetry/Attributes/TelemetryCollectionIdentityAttribute.cs
+++ ICD.Connect.Telemetry/Attributes/TelemetryCollectionIdentityAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 #if SIMPLSHARP
 using Crestron.SimplSharp.Reflection;
@@ -14,18 +15,47 @@ namespace ICD.Connect.Telemetry.Attributes
 	[AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
 	public sealed class TelemetryCollectionIdentityAttribute : AbstractIcdAttribute
 	{
+		private static readonly Dictionary<Type, PropertyInfo> s_IdentityCache;
+
+		/// <summary>
+		/// Static constructor.
+		/// </summary>
+		static TelemetryCollectionIdentityAttribute()
+		{
+			s_IdentityCache = new Dictionary<Type, PropertyInfo>();
+		}
+
+		/// <summary>
+		/// Gets the identity property for the given instance.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <returns></returns>
 		[CanBeNull]
 		public static PropertyInfo GetProperty([NotNull] object instance)
 		{
-			// todo - cache
+			if (instance == null)
+				throw new ArgumentNullException("instance");
+
+			Type type = instance.GetType();
+			return s_IdentityCache.GetOrAddNew(type, () => GetPropertyUncached(instance));
+		}
+
+		/// <summary>
+		/// Gets the identity property for the given instance.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <returns></returns>
+		[CanBeNull]
+		private static PropertyInfo GetPropertyUncached([NotNull] object instance)
+		{
 			return
 				instance.GetType()
-						.GetAllTypes()
+				        .GetAllTypes()
 #if SIMPLSHARP
 						.Select(t => t.GetCType())
 #endif
-						.SelectMany(t => t.GetProperties())
-						.SingleOrDefault(p => p.GetCustomAttributes(typeof(TelemetryCollectionIdentityAttribute), true).Any());
+				        .SelectMany(t => t.GetProperties())
+				        .SingleOrDefault(p => p.GetCustomAttributes(typeof(TelemetryCollectionIdentityAttribute), true).Any());
 		}
 	}
 }
