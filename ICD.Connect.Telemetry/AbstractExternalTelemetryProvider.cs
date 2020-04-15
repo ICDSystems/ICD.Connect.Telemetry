91d738bb8774eddde325053a5aa0620d3880ec57
Chris Cameron <chris.cameron@wedoresi.com>
2020-04-15 16:16:37 -0400

    feat: Adding virtual methods to external telemetry providers for subscribing to the parent provider

--- ICD.Connect.Telemetry/AbstractExternalTelemetryProvider.cs
+++ ICD.Connect.Telemetry/AbstractExternalTelemetryProvider.cs
@@ -3,12 +3,14 @@
 	public abstract class AbstractExternalTelemetryProvider<TParent> : AbstractTelemetryProvider, IExternalTelemetryProvider
 		where TParent : ITelemetryProvider
 	{
-		private TParent m_Provider;
+		private TParent m_Parent;
 
 		/// <summary>
 		/// Gets the parent telemetry provider.
 		/// </summary>
-		protected TParent Parent { get { return m_Provider; } }
+		protected TParent Parent { get { return m_Parent; } }
+
+		#region Methods
 
 		/// <summary>
 		/// Sets the parent telemetry provider that this instance extends.
@@ -22,10 +24,34 @@
 		/// <summary>
 		/// Sets the parent telemetry provider that this instance extends.
 		/// </summary>
-		/// <param name="provider"></param>
-		public void SetParent(TParent provider)
+		/// <param name="parent"></param>
+		public virtual void SetParent(TParent parent)
 		{
-			m_Provider = provider;
+			Unsubscribe(m_Parent);
+			m_Parent = parent;
+			Subscribe(m_Parent);
 		}
+
+		#endregion
+
+		#region Parent Callbacks
+
+		/// <summary>
+		/// Subscribe to the parent events.
+		/// </summary>
+		/// <param name="parent"></param>
+		protected virtual void Subscribe(TParent parent)
+		{
+		}
+
+		/// <summary>
+		/// Unsubscribe from the parent events.
+		/// </summary>
+		/// <param name="parent"></param>
+		protected virtual void Unsubscribe(TParent parent)
+		{
+		}
+
+		#endregion
 	}
 }
