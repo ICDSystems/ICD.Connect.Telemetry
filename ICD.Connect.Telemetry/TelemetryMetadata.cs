212cee56b06bfc99999568e4d4079b641b326b07
Chris Cameron <chris.cameron@wedoresi.com>
2020-04-24 16:27:05 -0400

    fix: Fixed a bug where telemetry metadata ranges were wrong

--- ICD.Connect.Telemetry/TelemetryMetadata.cs
+++ ICD.Connect.Telemetry/TelemetryMetadata.cs
@@ -1,32 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using ICD.Common.Properties;
 using ICD.Common.Utils;
+using ICD.Common.Utils.Attributes;
 using ICD.Common.Utils.Extensions;
 using ICD.Common.Utils.Json;
+using ICD.Connect.Telemetry.Nodes;
 using Newtonsoft.Json;
 
 namespace ICD.Connect.Telemetry
 {
+	[Flags]
+	public enum eMetadataSupport
+	{
+		None = 0,
+		EnumerationValues = 1,
+		Range = 2
+	}
+
 	[JsonConverter(typeof(TelemetryMetadataConverter))]
 	public sealed class TelemetryMetadata
 	{
-        /// <summary>
+		#region Properties
+
+		/// <summary>
 		/// Gets the datatype of the property this metadata is for 
 		/// </summary>
-		public Type DataType { get; set; }
+		public Type DataType { get; private set; }
 
 		/// <summary>
 		/// Sets which properties of this metadata should be serialized.
 		/// </summary>
-		public eMetadataSupport Supports { get; set; }
-
-		#region Enum
-		
-		/// <summary>
-		/// Returns if this metadata pertains to an enumeration
-		/// </summary>
-		public bool IsEnumeration { get { return DataType != null && DataType.IsEnum; } }
+		public eMetadataSupport Supports { get; private set; }
 
 		#endregion
 
@@ -35,21 +42,43 @@ namespace ICD.Connect.Telemetry
 		/// <summary>
 		/// get/set the minimum range
 		/// </summary>
-		public double RangeMin { get; set; }
+		public double RangeMin { get; private set; }
 
 		/// <summary>
 		/// get/set the maximum range
 		/// </summary>
-		public double RangeMax { get; set; }
-		#endregion
-	}
+		public double RangeMax { get; private set; }
 
-	[Flags]
-	public enum eMetadataSupport
-	{
-		None = 0,
-		EnumerationValues = 1,
-		Range = 2
+		#endregion
+
+		/// <summary>
+		/// Creates metadata for the given feedback telemetry.
+		/// </summary>
+		/// <param name="feedbackTelemetryItem"></param>
+		public static TelemetryMetadata FromFeedbackTelemetry([NotNull] IFeedbackTelemetryItem feedbackTelemetryItem)
+		{
+			TelemetryMetadata output = new TelemetryMetadata
+			{
+				DataType = feedbackTelemetryItem.ValueType
+			};
+
+			eMetadataSupport supports = eMetadataSupport.None;
+
+			if (feedbackTelemetryItem.ValueType.IsEnum)
+				supports = supports | eMetadataSupport.EnumerationValues;
+
+			RangeAttribute rangeAttr = feedbackTelemetryItem.PropertyInfo.GetCustomAttributes<RangeAttribute>().FirstOrDefault();
+			if (rangeAttr != null)
+			{
+				supports = supports | eMetadataSupport.Range;
+				output.RangeMin = (double)Convert.ChangeType(rangeAttr.Min, TypeCode.Double, CultureInfo.InvariantCulture);
+				output.RangeMax = (double)Convert.ChangeType(rangeAttr.Max, TypeCode.Double, CultureInfo.InvariantCulture);
+			}
+
+			output.Supports = supports;
+
+			return output;
+		}
 	}
 
 	public sealed class TelemetryMetadataConverter : AbstractGenericJsonConverter<TelemetryMetadata>
@@ -70,21 +99,20 @@ namespace ICD.Connect.Telemetry
 		{
 			base.WriteProperties(writer, value, serializer);
 
-			if (value.DataType == null)
-				return;
+			if (value.DataType != null)
+				writer.WriteProperty(TYPE_TOKEN, value.DataType.GetMinimalName());
 
-			writer.WriteProperty(TYPE_TOKEN, value.DataType.GetMinimalName());
-
-			if(value.Supports.HasFlag(eMetadataSupport.EnumerationValues))
+			if (value.Supports.HasFlag(eMetadataSupport.EnumerationValues))
 			{
-				Dictionary<int, string> enumStrings = EnumUtils.GetValues(value.DataType)
-				                                               .Select(e => new KeyValuePair<int, string>((int)e, e.ToString()))
-				                                               .ToDictionary();
+				Dictionary<int, string> enumStrings =
+					EnumUtils.GetValues(value.DataType)
+					         .ToDictionary(k => (int)k, k => k.ToString());
+
 				writer.WritePropertyName(ENUM_ITEMS_TOKEN);
 				serializer.SerializeDict(writer, enumStrings);
 			}
 
-			if(value.Supports.HasFlag(eMetadataSupport.Range))
+			if (value.Supports.HasFlag(eMetadataSupport.Range))
 			{
 				writer.WritePropertyName(RANGE_TOKEN);
 
@@ -93,20 +121,6 @@ namespace ICD.Connect.Telemetry
 				writer.WriteProperty(MAX_TOKEN, value.RangeMax);
 				writer.WriteEndObject();
 			}
-		}
-
-		/// <summary>
-		/// Override to handle the current property value with the given name.
-		/// </summary>
-		/// <param name="property"></param>
-		/// <param name="reader"></param>
-		/// <param name="instance"></param>
-		/// <param name="serializer"></param>
-		protected override void ReadProperty(string property, JsonReader reader, TelemetryMetadata instance, JsonSerializer serializer)
-		{
-			//Reading not required - these items are only for sending out to telemetry.
-
-			base.ReadProperty(property, reader, instance, serializer);
 		}
 	}
 }
