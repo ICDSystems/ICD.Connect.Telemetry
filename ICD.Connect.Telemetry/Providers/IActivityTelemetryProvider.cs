0f533ee927cd318797b592cf0370c394e1eb183d
Chris Cameron <chris.cameron@wedoresi.com>
2020-06-01 16:01:48 -0400

    feat: Initial commit of IActivityTelemetryProvider

---
+++ ICD.Connect.Telemetry/Providers/IActivityTelemetryProvider.cs
@@ -0,0 +1,19 @@
+﻿using ICD.Common.Logging.Activities;
+using ICD.Connect.Telemetry.Attributes;
+using ICD.Connect.Telemetry.Providers.External;
+
+namespace ICD.Connect.Telemetry.Providers
+{
+	[ExternalTelemetry("Activities", typeof(ActivityExternalTelemetryProvider))]
+	public interface IActivityTelemetryProvider : ITelemetryProvider
+	{
+		/// <summary>
+		/// Gets the activities for this instance.
+		/// </summary>
+		IActivityContext Activities { get; }
+	}
+
+	public sealed class ActivityExternalTelemetryProvider : AbstractExternalTelemetryProvider<IActivityTelemetryProvider>
+	{
+	}
+}
