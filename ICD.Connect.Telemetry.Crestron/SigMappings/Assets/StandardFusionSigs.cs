ccc7f473fc15617d64e4c36c1f679b09ff448c8d
Chris Cameron <chris.cameron@wedoresi.com>
2020-09-17 17:34:00 -0400

    refactor: Tidying

--- ICD.Connect.Telemetry.Crestron/SigMappings/Assets/StandardFusionSigs.cs
+++ ICD.Connect.Telemetry.Crestron/SigMappings/Assets/StandardFusionSigs.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using ICD.Common.Utils.Collections;
-using ICD.Connect.Devices.Controls;
 using ICD.Connect.Devices.Controls.Power;
 using ICD.Connect.Devices.Telemetry;
 using ICD.Connect.Devices.Telemetry.DeviceInfo.Monitored;
