26626e44fb8df6e569d7f65f06cf4aca259d8e1b
Chris Cameron <chris.cameron@wedoresi.com>
2020-10-06 19:15:37 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Telemetry.CrestronPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Telemetry.CrestronPro/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Telemetry.CrestronPro")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("5.4.0.0")]
+[assembly: AssemblyVersion("5.5.0.0")]
