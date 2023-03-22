e1f3c644dffcdf4183f9b958826fb274930def5d
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-22 08:18:35 -0400

    chore: Update copyright year

--- ICD.Connect.Telemetry.Services/Properties/AssemblyInfo.cs
+++ ICD.Connect.Telemetry.Services/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("ICD.Connect.Telemetry.Services")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Telemetry.Services")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
 [assembly: AssemblyVersion("8.5.4.0")]
